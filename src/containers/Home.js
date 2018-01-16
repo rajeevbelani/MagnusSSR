3407f53b209ee69ade2493c14c889af288fce2f8
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-01-16 13:11:04 +0530

    Removing grommet addons

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
 //
-import logoImg from '../logo.png'
+import PrimaryPage from '../components/PrimaryPage';
+// import logoImg from '../logo.png'
 
 export default getSiteProps(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <PrimaryPage />
+    {/* <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+    <img src={logoImg} alt="" /> */}
   </div>
 ))
