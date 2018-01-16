1a082198f7ed5585debcb81c19b5ac267bac2b3f
Rajeev Belani <rajeev@woodsofttechnologies.com>
2018-01-16 12:31:26 +0530

    first commit

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { getSiteProps } from 'react-static'
-import PrimaryPage from '../components/PrimaryPage'
+//
+import logoImg from '../logo.png'
 
 export default getSiteProps(() => (
   <div>
-    <PrimaryPage />
+    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+    <img src={logoImg} alt="" />
   </div>
 ))
